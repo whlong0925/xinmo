1450688892187b53f11afb16c909e0beae91b8a9
wanghuailong <wanghuailong@ucredit.com>
2016-04-15 15:07:39 +0800

    增加验证码â

--- src/main/java/com/xinmo/controller/IndexController.java
+++ src/main/java/com/xinmo/controller/IndexController.java
@@ -1,15 +1,21 @@
 package com.xinmo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.google.code.kaptcha.Constants;
 
 @Controller
 public class IndexController {
@@ -22,9 +28,17 @@ public class IndexController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public String login(Model model, @RequestParam("username") String username,
-            @RequestParam("password") String password) {
+            @RequestParam("password") String password,@RequestParam("kaptcha") String kaptcha,HttpServletRequest request) {
+    	
         // 就是代表当前的用户。
         Subject currentUser = SecurityUtils.getSubject();
+        Session session = currentUser.getSession();
+        String sessionKaptcha = (String) session.getAttribute(Constants.KAPTCHA_SESSION_KEY);
+        if (StringUtils.isEmpty(sessionKaptcha) || !kaptcha.equalsIgnoreCase(sessionKaptcha)) {
+        	model.addAttribute("errorMessage", "验证码错误");
+        	return "login";
+        }
+        
         //获取基于用户名和密码的令牌 
         UsernamePasswordToken token = new UsernamePasswordToken(username,
             password);
--- src/main/webapp/WEB-INF/view/login.jsp
+++ src/main/webapp/WEB-INF/view/login.jsp
@@ -5,7 +5,14 @@
   <form action="login" method="post">
 	    用户名：<input type="text" id="username" name="username" />
 	    密码:<input type="password" id="password" name="password" />
+	   验证码：<img src="kaptcha.jpg" /> <input type="text" name="kaptcha" value="" />
     <input type="submit" value="提交" />
     </form>
   </body>
+  <script src="${webRoot}/js/jquery.js"></script>
+  <script type="text/javascript">
+  $(function(){
+      $('#kaptchaImage').click(function () { $(this).attr('src', '/kaptcha.jpg?' + Math.floor(Math.random()*100) ); })
+  });
+  </script>
 </html>
